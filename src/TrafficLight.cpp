5dbc6d2d73cd0b07eb17c4aea5226aa258ca3add
Omar <o.samak.ext@tahaluf.ai>
2024-01-28 20:17:19 +0200

    feat: implement receive method

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -9,7 +9,11 @@ T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    // The received object should then be returned by the receive function.
+    std::unique_lock<std::mutex> ulock{_mutex};
+    _condition.wait(ulock, [this] { return !_queue.empty(); });
+    T msg = std::move(_messages.back());
+    _messages.pop_back();
 }
 
 template <typename T>
@@ -29,14 +33,18 @@ TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
 }
-/* 
+
 void TrafficLight::waitForGreen()
 {
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+    while(true) {
+        if(_messages.receive() == TrafficLightPhase::green)
+            return;
+    }
 }
-*/
+
 TrafficLight::TrafficLightPhase TrafficLight::getCurrentPhase()
 {
     return _currentPhase;
