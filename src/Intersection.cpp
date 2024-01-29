4484a7a75d234f71e784883a864268985d22a6fe
Omar <o.samak.ext@tahaluf.ai>
2024-01-29 09:23:53 +0200

    style: remove comments

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -110,7 +110,6 @@ void Intersection::setIsBlocked(bool isBlocked)
 // virtual function which is executed in a thread
 void Intersection::simulate() // using threads + promises/futures + exceptions
 {
-    // FP.6a : In Intersection.h, add a private member _trafficLight of type TrafficLight. At this position, start the simulation of _trafficLight.
      _trafficLight.simulate();
     // launch vehicle queue processing in a thread
     threads.emplace_back(std::thread(&Intersection::processVehicleQueue, this));
@@ -141,8 +140,6 @@ void Intersection::processVehicleQueue()
 
 bool Intersection::trafficLightIsGreen()
 {
-   // please include this part once you have solved the final project tasks
-
    if (_trafficLight.getCurrentPhase() == TrafficLightPhase::green)
        return true;
    else
